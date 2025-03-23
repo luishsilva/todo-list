f3a6fa317f4b632a0241fa32841dfde22dc6209c
luishsilva <luizhrick@gmail.com>
2025-03-22 22:50:47 -0600

    Move todo routes to a separate route file

---
+++ backend/routes/todoRoutes.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+
+const TodoModel = require("../models/Todo");
+
+router.get("/", async (request, response) => {
+  try {
+    const todos = await TodoModel.find();
+    response.status(200).json(todos);
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/", async (request, response) => {
+  try {
+    const newTodo = new TodoModel(request.body);
+    await newTodo.save();
+    response.status(201).json(newTodo);
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+});
+
+router.put("/:id", async (request, response) => {
+  try {
+    const updatedTodo = await TodoModel.findOneAndUpdate(
+      { id: request.params.id },
+      {
+        name: request.body.name,
+        isComplete: request.body.isComplete,
+      },
+      { new: true }
+    );
+
+    if (!updatedTodo) {
+      return response.status(404).json({ error: "Todo not found" });
+    }
+
+    response.status(200).json(updatedTodo);
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
+});
+
+router.delete("/:id", async (request, response) => {
+  try {
+    const deleteTodo = await TodoModel.findOneAndDelete({
+      id: request.params.id,
+    });
+
+    if (!deleteTodo) {
+      return response.status(404).json({ error: "Todo not found" });
+    }
+
+    response.json({ message: "Todo deleted successfully", deleteTodo });
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const TodoModel = require("./models/Todo");
+const todosRoutes = require("./routes/todoRoutes");
+// const TodoModel = require("./models/Todo");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/ratehub")
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB successfully");
   })
@@ -16,61 +18,7 @@ mongoose
     console.error("Failed to connect to MongoDB:", err);
   });
 
-app.get("/api", async (request, response) => {
-  try {
-    const todos = await TodoModel.find();
-    response.json(todos);
-  } catch (error) {
-    response.status(500).json({ error: error.message });
-  }
-});
-
-app.post("/api", async (request, response) => {
-  try {
-    const newTodo = new TodoModel(request.body);
-    await newTodo.save();
-    response.status(201).json(newTodo);
-  } catch (error) {
-    response.status(500).json({ error: error.message });
-  }
-});
-
-app.put("/api/:id", async (request, response) => {
-  try {
-    const updatedTodo = await TodoModel.findOneAndUpdate(
-      { id: request.params.id },
-      {
-        name: request.body.name,
-        isComplete: request.body.isComplete,
-      },
-      { new: true }
-    );
-
-    if (!updatedTodo) {
-      return response.status(404).json({ error: "Todo not found" });
-    }
-
-    response.json(updatedTodo);
-  } catch (err) {
-    response.status(500).json({ error: err.message });
-  }
-});
-
-app.delete("/api/:id", async (request, response) => {
-  try {
-    const deleteTodo = await TodoModel.findOneAndDelete({
-      id: request.params.id,
-    });
-
-    if (!deleteTodo) {
-      return response.status(404).json({ error: "Todo not found" });
-    }
-
-    response.json({ message: "Todo deleted successfully", deleteTodo });
-  } catch (error) {
-    response.status(500).json({ error: error.message });
-  }
-});
+app.use("/api", todosRoutes);
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
