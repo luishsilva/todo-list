2f78c83be06eb92cebe3ca676da25a0c9646965c
luishsilva <luizhrick@gmail.com>
2025-03-25 17:20:16 -0600

    Prettier identation

--- frontend/src/components/TodoListItem.jsx
+++ frontend/src/components/TodoListItem.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './TodoListItem.css';
-
+import "./TodoListItem.css";
 
 export default function TodoListItem({ name, onComplete, onDelete, onChange }) {
-    return (
-        <li className="todo-list-item">
-            <input onChange={onChange} value={name} />
-            <button onClick={onComplete}>Complete</button>
-            <button onClick={onDelete}>DELETE</button>
-        </li>
-    )
+  return (
+    <li className='todo-list-item'>
+      <input onChange={onChange} value={name} />
+      <button onClick={onComplete}>Complete</button>
+      <button onClick={onDelete}>DELETE</button>
+    </li>
+  );
 }
